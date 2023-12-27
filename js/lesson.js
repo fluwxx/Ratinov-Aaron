f6f78296a7d41c9124abed55cd8f9e441bd841e6
fluwxx <aaronratinov@gmail.com>
2023-12-27 13:13:52 +0600

    commit

--- js/lesson.js
+++ js/lesson.js
@@ -170,3 +170,53 @@ converter(som, usd, 'som');
 converter(usd, som, 'usd');
 converter(eur, som, 'eur');
 converter(eur, usd, 'eur');
+
+
+// card switcher
+
+
+const card = document.querySelector('.card');
+const btnNext = document.querySelector('#btn-next');
+const btnPrev = document.querySelector('#btn-prev');
+
+let countCard = 1;
+
+const fetchCardData = (cardNumber) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${cardNumber}`)
+        .then(response => response.json())
+        .then((data)=>{
+            card.innerHTML = `
+                <p>${data.title}</p>
+                <p style="color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
+            `;
+            console.log(data);
+        });
+};
+
+const handleNextClick = () => {
+    countCard = (countCard % 200) + 1;
+    fetchCardData(countCard);
+};
+
+const handlePrevClick = () => {
+    countCard = (countCard === 1) ? 200 : countCard - 1;
+    fetchCardData(countCard);
+};
+
+fetchCardData(countCard);
+
+btnNext.addEventListener('click', handleNextClick);
+btnPrev.addEventListener('click', handlePrevClick);
+
+
+const fetchAndDisplayPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+        });
+};
+
+fetchAndDisplayPosts();
+
+
